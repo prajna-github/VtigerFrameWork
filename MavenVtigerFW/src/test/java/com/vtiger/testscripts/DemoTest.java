8d80a05b1f2088b4d43f278423c2e30d1416d062
Gaurav Rajendra Gaud <gaurav17.gaud@gmail.com>
2020-06-22 14:17:31 +0530

    New Class Added by gaurav

---
+++ MavenVtigerFW/src/test/java/com/vtiger/testscripts/DemoTest.java
@@ -0,0 +1,12 @@
+package com.vtiger.testscripts;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void checkDemo()
+	{
+		System.out.println("Demo running");
+	}
+}
